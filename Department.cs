5308ae5f3860b33fbbde77362f7250d144437029
Vinicius Borges <49001223+PopxX@users.noreply.github.com>
2022-08-19 14:39:03 -0300

    Add files via upload
    Entities

---
+++ Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercicio1.Entities
+{
+    internal class Department
+    {
+        public string Name { get; set; }    
+
+        public Department()
+        {
+
+        }
+
+        public Department(String name)
+        {
+            Name = name;
+        }
+
+
+    }
+}
